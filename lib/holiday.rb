9231e6acde669db91c92c40242eb65b5fc3662e2
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-09 03:42:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash[:holiday_supplies]
+holiday_hash.each do |holiday, supplies|
+  puts "#{holiday}: #{supplies}"
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
